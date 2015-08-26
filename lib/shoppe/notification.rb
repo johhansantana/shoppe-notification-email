588aa0094d7f00eda6c5054f696f7da00582cf05
jsantana <johhan.santana@hotmail.com>
2015-08-26 15:29:14 -0400

    first push

---
+++ lib/shoppe/notification.rb
@@ -0,0 +1,13 @@
+require "shoppe/notification/version"
+require "shoppe/notification/engine"
+
+module Shoppe
+  module Notification
+    # Your code goes here...
+    def self.setup
+		  Shoppe::Order.before_confirmation do
+		    Shoppe::NotificationMailer.order_received(self).deliver_now
+		  end
+		end
+  end
+end
